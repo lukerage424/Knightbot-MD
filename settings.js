1f9282d98bbfcc62be6eb774bfb8181a4f17f5a1
lukerage424 <manfredsevenzayi77@gmail.com>
2025-05-04 00:36:34 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'krugar knight',
   author: '‎',
   botName: "krugar knight",
-  botOwner: 'Professor', // Your name
+  botOwner: 'mannie krugar', // Your name
   ownerNumber: '26778615687', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
